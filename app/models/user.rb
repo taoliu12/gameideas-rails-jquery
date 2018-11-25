7f8f3ff3556adbd8d3f831c9dcba10eada015d94
kev112 <werewaswas@live.com>
2018-11-24 23:14:11 -0800

    add username validations messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: :true
-    validates :username, uniqueness: :true
+    validates :username, :presence => {:message => "Title can't be blank." }
+    validates :username, uniqueness: {:message => 'Username already taken. Please use another username.'}
 end

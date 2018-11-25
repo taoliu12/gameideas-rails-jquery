8ed9b84fbe49d8a3e541cb529aabeac267f72041
kev112 <werewaswas@live.com>
2018-11-24 20:49:27 -0800

    complete basic signup process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
     def new
-        
+        @user = User.new
     end
 
     def create
+        @user = User.create(user_params)
+        session[:user_id] = @user.id
+        redirect_to root_path
+    end
 
+    private
+
+    def user_params
+        params.require(:user).permit([:username, :password, :password_confirmation])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'application#welcome'
+
   resources :users, :only =>  [:new, :create]
 end

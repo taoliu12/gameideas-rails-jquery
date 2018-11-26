be76294d9d9e96681961683b23e458d47ef9063d
kev112 <werewaswas@live.com>
2018-11-25 18:49:09 -0800

    add basic sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, :presence => {:message => "Title can't be blank." }
+    validates :username, presence: {:message => "Username can't be blank." }
     validates :username, uniqueness: {:message => 'Username already taken. Please use another username.'}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'application#welcome'
+  root 'site#welcome'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 
   resources :users, :only =>  [:new, :create]
 end

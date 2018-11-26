880debcac558e07719e2d519dfdd25cc290b9790
kev112 <werewaswas@live.com>
2018-11-26 14:24:12 -0800

    add games actions index and new

--- db/migrate/20181126203846_create_games.rb
+++ db/migrate/20181126203846_create_games.rb
@@ -1,7 +1,10 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-
+      t.string :title
+      t.string :summary
+      t.integer :user_id
+      
       t.timestamps
     end
   end

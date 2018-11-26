37b34db99b4bab34f974b6721358d0cdfcca23d2
kev112 <werewaswas@live.com>
2018-11-25 18:59:12 -0800

    add basic sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
     def welcome
-        if session[:id]
-            @user = User.find(session[:id])
+        if session[:user_id].present?
+            @user = User.find(session[:user_id])
+        else
+            redirect_to login_path
         end
     end
 end

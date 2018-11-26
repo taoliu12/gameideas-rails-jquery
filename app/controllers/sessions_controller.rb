4c5585733c713829b174e097c39a19a13fb88d7c
kev112 <werewaswas@live.com>
2018-11-25 22:31:39 -0800

    add sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     end
   
     def create
-      @user = User.find_by(name: params[:user][:name])
+      @user = User.find_by(username: params[:user][:username])
   
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id

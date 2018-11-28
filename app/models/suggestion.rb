7d36b953c2d551f56cd9cf737e834ae2716607de
kev112 <werewaswas@live.com>
2018-11-27 22:26:51 -0800

    add jquery checkbox submit

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,5 +1,12 @@
 class Suggestion < ApplicationRecord
     belongs_to :game
-
     validates :content, :presence => true
+
+    def greenlit?
+        self.status == 1
+    end
+
+    def pending?
+        self.status == 0
+    end
 end

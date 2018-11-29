8eab3c4f2dd9ee83bcc60e4046e1cf2674cf5551
kev112 <werewaswas@live.com>
2018-11-29 14:28:35 -0800

    insert greenlit helpers

--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -1,2 +1,13 @@
 module SuggestionsHelper
+    def class_for_suggestion_li(suggestion)
+        "'greenlit'" if suggestion.greenlit?
+    end
+
+    def greenlit_or_pending(suggestion)
+        if suggestion.greenlit?
+            'Greenlit'
+        else
+            'Pending'       
+        end
+    end
 end

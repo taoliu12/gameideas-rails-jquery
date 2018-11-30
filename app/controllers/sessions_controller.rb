daf26353c6fea1c5fad4825441347811b22a1e00
kev112 <werewaswas@live.com>
2018-11-30 12:31:10 -0800

    fix client_id in omniauth.rb initializer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,14 @@ class SessionsController < ApplicationController
     end
   
     def create
+      raise "stop".inspect
       omniauth_email = request.env['omniauth.auth'][:info][:email] 
-      if auth_email
+      raise omniauth_email.inspect
+      if omniauth_email #omniauth login
         user = User.find_or_create_by_omniauth(omniauth_email) #custom omniauth helper
         session[:user_id] = user.id 
         redirect_to root_path 
-      else
+      else #native login
         @user = User.find_by(username: params[:user][:username])
     
         if @user && @user.authenticate(params[:user][:password])
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, ENV['60c0adfc797e28838bfe'], ENV['6a0b3d65100850e1a8be4bb8a1e3678566708c56']
+    provider :github, 'fb59f06792bc0fa6c5a2', 'a1b3f9230a7fd829db1acf59999b37d32fa6681e'
   end

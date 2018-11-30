3b27e16ba4c21f7855848bee970f066ee905314d
kev112 <werewaswas@live.com>
2018-11-29 21:31:26 -0800

    add current_user helper

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
 
   resources :users, :only =>  [:new, :create]
 end

033282fad3c71a7a4b5d633cf53762b65ddfb078
kev112 <werewaswas@live.com>
2018-11-30 13:12:27 -0800

    stub temp username for omniauth login, need to fix later

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
     validates :username, uniqueness: {:message => 'Username already taken. Please use another username.'}
 
     def self.find_or_create_by_omniauth(auth_email)
-        user = User.find_by_id(auth_email)
+        user = User.find_by(:email => auth_email)
         if user
             user
         else
             user = User.new(email: auth_email)
+            user.username = 'fixlater'
             user.password = SecureRandom.hex
             user.save 
             user  

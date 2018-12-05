083862d4e9396b9d4139bb7bb0ba71191ffdddb4
kev112 <werewaswas@live.com>
2018-12-04 18:31:53 -0800

    add suggested-users in games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
     belongs_to :user
+    belongs_to :genre
     has_many :suggestions
+    
     validates :title, :presence => true
     validates :summary, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_many :games
+    has_many :genres, through: :games
     has_many :suggestions 
     has_many :suggested_games, through: :suggestions, :source => :game
-
+    
     has_secure_password
     validates :username, presence: {:message => "Username can't be blank." }
     validates :username, uniqueness: {:message => 'Username already taken. Please use another username.'}

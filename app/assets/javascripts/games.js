120d3f70658707182dc64cf7f42a824f4b264ae9
kev112 <werewaswas@live.com>
2018-12-13 21:59:14 -0800

    add load suggestions

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,5 +1,10 @@
 $(function() {
     $('.greenlit-chkbox').on('change', function() {
         $(this).parents('form').trigger('submit')
-    })
+    });
+
+    $('a.load_suggestions').on('click', function(e) {
+        alert('Yo');
+        e.preventDefault();
+    });
 });

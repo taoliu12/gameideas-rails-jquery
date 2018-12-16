5341604808f4f7d7f1e4343506f1c4efcd7d3d0b
kev112 <werewaswas@live.com>
2018-12-15 22:45:31 -0800

    add form event js

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,10 +1,11 @@
 $(function() {
+    //Suggestions Checkbox 
     $('.greenlit-chkbox').on('change', function() {
         $(this).parents('form').trigger('submit')
     });
 
+    //Load suggestions 
     $('a.load_suggestions').on('click', function(e) {
-
         $.get(this.href).success(function(response) {
             $ol = $('div.suggestions ol') //good practice to use $ to prepend vars that ref jquery objects
             $ol.html("") 
@@ -16,7 +17,23 @@ $(function() {
         // debugger
         e.preventDefault(); 
         // e.stopImmediatePropagation();
-         
+    });
 
+    //Post suggestion
+    $('#new_suggestion').on('submit', function(e) {
+        // $.post({  
+        // }).success(function(response) {
+
+        // });
+        // debugger
+        url = this.action
+        data = {
+            'authenticity_token': $("input[name='authenticity_token']").attr('value'),
+            'suggestion': { 
+                'content':  $("#suggestion_content").val()
+            }
+        }
+        console.log('data');
+        e.preventDefault(); 
     });
 });

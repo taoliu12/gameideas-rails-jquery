b67cb3aefcc9093db670d179fb7d464fa277ee5c
kev112 <werewaswas@live.com>
2018-12-29 14:08:56 -0800

    sort suggestions by newest

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,7 +2,9 @@ class Suggestion < ApplicationRecord
     belongs_to :game
     belongs_to :user
     validates :content, :presence => true
-    scope :recent, -> { all.limit(5) }
+    
+    scope :newest_to_oldest, -> { order('created_at DESC') }
+    scope :recent, -> { newest_to_oldest.limit(5) }
 
     def greenlit?
         self.status == 1

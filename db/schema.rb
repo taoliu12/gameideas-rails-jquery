48cc1bfa00c28c6238175780efb832581b1ebd27
taoliu12 <taoliudesign@gmail.com>
2021-01-24 13:25:19 -0800

    fix view

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_12_063109) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string "title"
     t.string "summary"

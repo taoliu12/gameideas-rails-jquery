bc946b138aaf1a8603477c68ba032d9c91756b8d
taoliu12 <taoliudesign@gmail.com>
2021-01-28 13:33:59 -0800

    fix current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
+        return unless session[:user_id]
         @current_user ||= User.find(session[:user_id])
     end
 

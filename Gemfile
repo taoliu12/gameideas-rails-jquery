8b8b807ce8db21fe178e712b692fc3eedee48e12
Tao Liu <werewaswas@live.com>
2023-03-08 21:32:13 -0800

    upgrade to rails 6
    add db seeds

--- Gemfile
+++ Gemfile
@@ -1,21 +1,21 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.6'
+ruby '2.7.0'
 
-gem 'omniauth'
-gem 'omniauth-github'
+# gem 'omniauth'
+# gem 'omniauth-github'
 gem 'active_model_serializers'
 gem 'dotenv-rails'
 
 gem 'bcrypt'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.1'
+gem 'rails', '~> 6.0'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
 gem 'pg'
 # Use Puma as the app server
-gem 'puma', '~> 3.11'
+gem 'puma'
 # Use SCSS for stylesheets
 gem 'materialize-sass'
 gem 'sass-rails', '~> 5.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+users = User.create(
+  [{username: 'user1', password: 'password'}, {username: 'user2', password: 'password'}, {username: 'user3', password: 'password'}, {username: 'user4', password: 'password'}, {username: 'aaaa', password: 'aaaa'}
+  ]
+)
 
 Genre.create(name: 'Action/Adventure')
 Genre.create(name: 'Platformer')
@@ -16,3 +22,30 @@ Genre.create(name: 'Survival Horror')
 Genre.create(name: 'First Person Shooter')
 Genre.create(name: 'Simulator')
 Genre.create(name: 'Fighting')
+
+# copied from other app:
+# users.each do |user|
+#     user_posts = [] 
+#      10.times {user_posts << user.authored_posts.create(
+#         { 
+#           title: Faker::Lorem.sentence, 
+#           body: Faker::Lorem.paragraph(sentence_count: rand(1..21))
+#         }
+#       )
+#      }
+
+#     user_posts.each do |post|
+#         Vote.create(user_id: user.id, post_id: post.id, value: 1)
+#     end
+
+#     Post.all.each do |post|
+#       3.times {
+#         post.comments.create(
+#           { 
+#             user_id: user.id, 
+#             content: Faker::Lorem.paragraph(sentence_count: rand(1..21))
+#           }
+#         )
+#       }
+#     end
+# end
